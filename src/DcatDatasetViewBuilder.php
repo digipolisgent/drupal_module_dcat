039b60a4a59468d9bda6eacb0e5445375add7165
AlonaOneill <AlonaOneill@3541269.no-reply.drupal.org>
2018-09-05 13:44:58 +0200

    Issue #2994405 by alonaoneill, daniel_rose: Dependency namespacing in .info.yml file

--- src/DcatDatasetViewBuilder.php
+++ src/DcatDatasetViewBuilder.php
@@ -5,7 +5,6 @@ namespace Drupal\dcat;
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityViewBuilder;
-use Drupal\node\Entity\Node;
 
 /**
  * View builder handler for datasets.
@@ -16,7 +15,7 @@ class DcatDatasetViewBuilder extends EntityViewBuilder {
    * {@inheritdoc}
    */
   protected function alterBuild(array &$build, EntityInterface $entity, EntityViewDisplayInterface $display, $view_mode) {
-    /** @var \Drupal\node\NodeInterface $entity */
+    /** @var \Drupal\Core\Entity\EntityInterface $entity */
     parent::alterBuild($build, $entity, $display, $view_mode);
     if ($entity->id()) {
       $build['#contextual_links']['dcat_dataset'] = [
